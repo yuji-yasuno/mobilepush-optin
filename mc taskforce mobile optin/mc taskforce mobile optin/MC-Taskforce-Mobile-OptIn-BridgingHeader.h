b01e209b62d1271d282ecdd50e94d04711a85858
Yuji Yasuno <yuji.yasuno@gmial.com>
2016-01-21 14:02:46 +0900

    initial commit

---
+++ mc taskforce mobile optin/mc taskforce mobile optin/MC-Taskforce-Mobile-OptIn-BridgingHeader.h
@@ -0,0 +1,25 @@
+//
+//  MC-Taskforce-Mobile-OptIn-BridgingHeader.h
+//  mc taskforce mobile optin
+//
+//  Created by 楊野勇智 on 2016/01/15.
+//  Copyright © 2016年 salesforce.com. All rights reserved.
+//
+
+#ifndef MC_Taskforce_Mobile_OptIn_BridgingHeader_h
+#define MC_Taskforce_Mobile_OptIn_BridgingHeader_h
+
+
+#endif /* MC_Taskforce_Mobile_OptIn_BridgingHeader_h */
+
+#import "ETGenericUpdate.h"
+#import "ETKeyValueStore.h"
+#import "ETLandingPagePresenter.h"
+#import "ETLocationManager.h"
+#import "ETLocationUpdate.h"
+#import "ETMessage.h"
+#import "ETPush.h"
+#import "ETRegion.h"
+#import "ETSqliteHelper.h"
+#import "ExactTargetEnhancedPushDataSource.h"
+#import "PushConstants.h"
---
+++ mc taskforce mobile optin/mc taskforce mobile optin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mc taskforce mobile optin
+//
+//  Created by 楊野勇智 on 2016/01/15.
+//  Copyright © 2016年 salesforce.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
